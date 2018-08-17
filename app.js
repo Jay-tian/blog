1f495a0af6c2e0225b45eefd432ac4dae0fe53a5
tianshengjie <tianshengjie@howzhi.com>
2018-08-17 10:04:51 +0800

    delete useless coce

---
+++ app.js
@@ -0,0 +1,2 @@
+const app = require('koa-symphony/src/server/kernel.js');
+app.listen(3000);
--- src/server/twig-extension/common-filter.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  test: function(path) {
-    return path+'?v=1.0.0';
-  },
-  upperCase: function(s) {
-    return s.toUpperCase();
-  }
-};
