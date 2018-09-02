eb214a899c900337d277b23d00416e5532ea20af
tianshengjie <tianshengjie@howzhi.com>
2018-09-02 20:34:02 +0800

    my-article

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -4,3 +4,10 @@ require('bootstrap');
 window.app = {
   version: version,
 };
+
+$('.js-delete').on('click', function() {
+  let url = $(this).data('url');
+  $.post(url, function() {
+    alert(123);
+  });
+});
