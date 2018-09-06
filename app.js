577397be19f6d3af7c3f9c25797f888a4e046402
tianshengjie <tianshengjie@howzhi.com>
2018-09-06 22:24:01 +0800

    update delete

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-const app = require('koa-symphony/src/kernel.js');
+let app = require('koa-symphony/src/kernel.js');
 app.listen(3000);
+
+// let app = require('koa-symphony/src/kernel.js');
+// var Router = require('koa-router');
+// var router = new Router();
+
+ 
+// const websockify = require('koa-websocket');
+// const wsOptions = {};
+// app = websockify(app, wsOptions);
+ 
+// router.get('websocket', (ctx) => {
+//   ctx.websocket.on('message', function(message) {
+//     console.log(message);
+//   });
+// });
+
+
+// app.ws.use(router.allowedMethods());
+// app.listen(3000);
