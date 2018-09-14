47fb670826c81cd70f779e13e09a3d0b494a5f02
tianshengjie <tianshengjie@howzhi.com>
2018-09-14 14:47:01 +0800

    u

--- src/server/dao/system/SessionDao.js
+++
@@ -1,9 +0,0 @@
-const BaseDao = require('koa-symphony/src/dao/BaseDao.js');
-
-class SessionDao extends BaseDao{
-  constructor(){
-    super('session');
-  }
-}
-
-module.exports = new SessionDao();
