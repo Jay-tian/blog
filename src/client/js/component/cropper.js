e10c8ab4fede60eb350364806b7cb6c6249daa21
tianshengjie <tianshengjie@howzhi.com>
2018-09-19 11:15:05 +0800

    update

--- src/client/js/component/cropper.js
+++ src/client/js/component/cropper.js
@@ -1,7 +1,21 @@
 require('tsj-jcrop/js/Jcrop.min.js');
 
 module.exports = function($element) {
-  
+  // $box = $('<div id="cropper" class="cropper"></div>');
+  if ($('body').find('#cropper').length) {
+    return;
+  }
+
+  let img = new Image();
+  img.src = $element.attr('src');
+  if (img.complete) {
+    console.log();
+  } else {
+    img.onload = function () {
+      console.log();
+    };
+  }
+
   $element.Jcrop({
     trueSize: [200, 200],
     setSelect: [0, 0, 10, 10],
