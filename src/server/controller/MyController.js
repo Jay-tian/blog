098c6212f72741ca811c1f4ccaebdf5d001eb643
tianshengjie <tianshengjie@howzhi.com>
2019-02-25 09:32:00 +0800

    新增分页组件

--- src/server/controller/MyController.js
+++ src/server/controller/MyController.js
@@ -1,4 +1,5 @@
 const BaseController = require('koa-symphony/src/controller/BaseController');
+const Pagination = require('../toolkit/Pagination');
 
 class MyController extends BaseController {
   constructor(){
@@ -9,10 +10,17 @@ class MyController extends BaseController {
     return async (ctx) => {
       let user = ctx.state.user;
       let count = await this.articleService().count({userId: user.get('id')});
-      let articles = await this.articleService().findArticlesByUserId(user.get('id'), 0, count);
-      
+      // 中间件处理
+      if (!ctx.request.query.page) {
+        ctx.request.query.page = 1;
+      }
+
+      let articles = await this.articleService().findArticlesByUserId(user.get('id'), (ctx.request.query.page - 1) * Pagination.pageCount, Pagination.pageCount);
+      let pagination = new Pagination(ctx.request, count);
+
       return ctx.render('article/my-articles.twig', {
-        articles: articles
+        articles: articles,
+        pagination: pagination,
       });
     };    
   }
